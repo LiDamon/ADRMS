fa02cc99547a1e09096e0acf4fb352d35f0de0f3
Ahmad Alhour <aalhour@ccc.gr>
2014-01-20 07:02:11 +0000

    Added UserRightsHelper class to the project. POC Program.cs

--- RMSClient/RMSClient/Program.cs
+++ RMSClient/RMSClient/Program.cs
@@ -25,61 +25,113 @@ namespace RMSClient
 
         static void Main(string[] args)
         {
-        //    Console.Clear();
-        //    Console.WriteLine("* Start of Transmission");
+            //Console.Clear();
+            //Console.WriteLine("* Start of Transmission");
+
+            ////String inputFile = "docxFile.docx";
+            //String inputFile = @"ptestFile.pdf";
+
+            //var encryptor = new EncryptionAndDecryption();
+
+            ////Collection<TemplateInfo> myTemplateInfo = encryptor.GetTemplatesInfo();
+
+            //bool inputFileIsEncrypted = encryptor.IsEncrypted(filePath + @"\" + inputFile);
+
+            //if (inputFileIsEncrypted)
+            //{
+            //    Console.WriteLine("** DECRYPT: File " + inputFile + " is encrypted");
+
+            //    encryptor.DecryptFile(filePath + @"\" + inputFile);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("** File " + inputFile + " is not encrypted");
+
+            //    string owner = "sghaida@ccc.gr";
+            //    string user = "aalhour@ccc.gr";
+
+            //    var rights = new Collection<UserRights>();
+
+            //    UserRights owner_rights = encryptor.SetUserRights(UserIdType.Email, owner, new Collection<string>() 
+            //    {
+            //        CommonRights.OwnerRight
+            //    });
+
+            //    UserRights user_rights = encryptor.SetUserRights(UserIdType.Email, user, new Collection<string>() 
+            //    {
+            //        CommonRights.ViewRightsDataRight,
+            //        CommonRights.ViewRight,
+            //        CommonRights.PrintRight,
+            //        CommonRights.ForwardRight
+            //    });
+
+            //    rights.Add(owner_rights);
+            //    rights.Add(user_rights);
+
+            //    encryptor.EncryptFile(owner, rights, filePath + @"\" + inputFile);
+            //    encryptor.EncryptFile(filePath + @"\" + inputFile, myTemplateInfo[0].TemplateId);
+
+            //}//End-Else
+
             
-        //    //String inputFile = "docxFile.docx";
-        //    String inputFile = @"ptestFile.pdf";
-
-        //    var encryptor = new EncryptionAndDecryption();
-
-        //    //Collection<TemplateInfo> myTemplateInfo = encryptor.GetTemplatesInfo();
-
-        //    bool inputFileIsEncrypted =encryptor.IsEncrypted(filePath + @"\" + inputFile);
-            
-        //    if (inputFileIsEncrypted)
-        //    {
-        //        Console.WriteLine("** DECRYPT: File " + inputFile + " is encrypted");
-
-        //        encryptor.DecryptFile(filePath + @"\" + inputFile);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("** File " + inputFile + " is not encrypted");
-
-        //        string owner = "sghaida@ccc.gr";
-        //        string user = "aalhour@ccc.gr";
-
-        //        var rights = new Collection<UserRights>();
-
-        //        UserRights owner_rights = encryptor.SetUserRights(UserIdType.Email, owner, new Collection<string>() 
-        //        {
-        //            CommonRights.OwnerRight
-        //        });
-
-        //        UserRights user_rights = encryptor.SetUserRights(UserIdType.Email, user, new Collection<string>() 
-        //        {
-        //            CommonRights.ViewRightsDataRight,
-        //            CommonRights.ViewRight,
-        //            CommonRights.PrintRight,
-        //            CommonRights.ForwardRight
-        //        });
-                
-        //        rights.Add(owner_rights);
-        //        rights.Add(user_rights);
-
-              //encryptor.EncryptFile(owner, rights, filePath + @"\" + inputFile);
-                //encryptor.EncryptFile(filePath + @"\" + inputFile, myTemplateInfo[0].TemplateId);
-
-        //    }//End-Else
-
+            var encryptionAndDecryption = new EncryptionAndDecryption();
             var options = new Options();
+
 
             if (CommandLine.Parser.Default.ParseArguments(args, options)) 
             {
+                string action = options.action.ToLower();
+                string file = options.InputFile.ToLower();
+                string templateName = string.Empty;
+                Collection<UserRights> usersRightsCollection = new Collection<UserRights>();
+                Collection<TemplateInfo> templatesInfo = encryptionAndDecryption.GetTemplatesInfo();
+
                 if (options.action.ToLower() == "protect" || options.action.ToLower() == "unprotect")
                 {
                     Console.WriteLine("Action: {0} File", options.action);
+
+                    //PROTECT CASE
+                    if (action == "protect" && encryptionAndDecryption.IsEncrypted(file) == false)
+                    {
+                        //IN CASE OF RIGHTS LIST
+                        if (!string.IsNullOrEmpty(options.rights))
+                        {
+                            usersRightsCollection = UserRightsHelper.ConvertRightsStringToCollection(options.rights);
+
+                            encryptionAndDecryption.EncryptFile("aalhour@ccc.gr", usersRightsCollection, file);
+                        }
+
+                        //IN CASE OF TEMPLATE NAME
+                        else if (!string.IsNullOrEmpty(options.templateName))
+                        {
+                            templateName = options.templateName.ToLower();
+
+                            var template = templatesInfo.FirstOrDefault(item => item.Name.ToLower() == templateName);
+
+                            if (template != null)
+                            {
+                                encryptionAndDecryption.EncryptFile(file, template.TemplateId);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Template does not exist, please choose an available template name.");
+                                Console.WriteLine(options.GetUsage());
+                            }
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Please pass either a rights string or a template name.");
+                            Console.WriteLine(options.GetUsage());
+                        }
+
+                        //Console.WriteLine("Action was completed successfully.");
+                    }
+                    else if (action == "unprotect" && encryptionAndDecryption.IsEncrypted(file) == true)
+                    {
+                        encryptionAndDecryption.DecryptFile(file);
+                        //Console.WriteLine("Action was completed successfully.");
+                    }
                 }
                 else 
                 {
---
+++ RMSClient/RMSClient/UserRightsHelper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace CCC.RMSLib
+{
+    class UserRightsHelper
+    {
+        string EmailAddress { get;set; }
+        string RightsList { get; set; }
+
+
+        public void AddRights(string EmailAddress, string RightsList)
+        {
+            this.EmailAddress = EmailAddress;
+            this.RightsList = RightsList;
+        }
+
+
+        public static string ConvertRightsToString(UserRightsHelper rights) 
+        {
+           StringBuilder sb = new StringBuilder();
+
+            sb.Append(rights.EmailAddress);
+            sb.Append(":");
+            sb.Append(rights.RightsList);
+
+            return sb.ToString();
+
+        }
+
+
+        public static UserRightsHelper ConvertStringtoRights(string rights)
+        {
+            UserRightsHelper userRights = new UserRightsHelper();
+
+            string[] rightsArray = rights.Split(':');
+
+            userRights.AddRights(rightsArray[0], rightsArray[1]);
+
+            return userRights;
+        }
+
+
+        public static Collection<UserRights> ConvertRightsStringToCollection(string rights)
+        {
+            UserRights userRightObject;
+            Collection<string> commonRights;
+            Collection<UserRights> usersRightsCollection = new Collection<UserRights>();
+            Collection<string> SystemCommonRights = GetCommonRights();
+
+            List<string> tempUserRights = new List<string>();
+
+            try
+            {
+                tempUserRights = rights.ToLower().Split(';').ToList();
+            }
+            catch (Exception e)
+            {
+            }
+
+            //foreach user
+            foreach (string tempUserRight in tempUserRights)
+            {
+                commonRights = new Collection<string>();
+
+                var userRightsInfo = ConvertStringtoRights(tempUserRight);
+
+                if (!string.IsNullOrEmpty(userRightsInfo.RightsList))
+                {
+                    var rightsList = userRightsInfo.RightsList.Split(',').ToList();
+
+                    foreach (string right in rightsList)
+                    {
+                        if (SystemCommonRights.Contains(right.ToUpper()))
+                            commonRights.Add(right.ToUpper());
+                    }
+
+                    userRightObject = new UserRights(UserIdType.Email, userRightsInfo.EmailAddress, commonRights);
+                        
+                    usersRightsCollection.Add(userRightObject);
+                }
+            }
+
+            return usersRightsCollection;
+        }
+
+
+        public static Collection<string> GetCommonRights()
+        {
+            Collection<string> commonRights = new Collection<string>()
+            {
+                CommonRights.OwnerRight,
+                CommonRights.ViewRight,
+                CommonRights.EditRight,
+                CommonRights.ExtractRight,
+                CommonRights.ExportRight,
+                CommonRights.PrintRight,
+                CommonRights.CommentRight,
+                CommonRights.ViewRightsDataRight,
+                CommonRights.EditRightsDataRight,
+                CommonRights.ForwardRight,
+                CommonRights.ReplyRight,
+                CommonRights.ReplyAllRight,
+                CommonRights.ObjectModelRight,
+                CommonRights.DocEditRight
+            };
+
+            return commonRights;
+        }
+
+    }
+
+    
+}
